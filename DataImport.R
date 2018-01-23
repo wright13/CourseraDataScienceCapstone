3908ca45129625d6cdc167659d278a45a0dd3caf
Sarah Wright <s.wright.618@gmail.com>
2018-01-23 10:05:35 -0800

    Modified to read training data into several smaller files so that tokenization can be done iteratively

--- DataImport.R
+++ DataImport.R
@@ -1,70 +1,55 @@
-DataImport <- function(working.dir) {
-    # Load packages
-    library(data.table)
-    library(tidyverse)
-    library(quanteda)
-    library(readr)
-    library(R.utils)
-    
-    #setwd("M:/STAFF/WrightS/Classes/Data Science Capstone")
-    setwd(working.dir)
+# Load packages
+library(data.table)
+library(tidyverse)
+library(quanteda)
+library(readr)
+library(R.utils)
+
+#setwd("M:/STAFF/WrightS/Classes/Data Science Capstone")
 
 
-    # Unzip the dataset
-    #unzip("Coursera-Swiftkey.zip", exdir = "data", overwrite = FALSE, setTimes = FALSE)
-    path.to.data <- paste0(getwd(), "/data/final/en_US/")
-    
-    # Create a read connection to each English dataset (Twitter, news, and blogs)
-    blogs.con <- file(paste0(path.to.data, "en_US.blogs.txt"), "rb", encoding = "UTF-8")
-    news.con <- file(paste0(path.to.data, "en_US.news.txt"), "rb", encoding = "UTF-8")
-    twitter.con <- file(paste0(path.to.data, "en_US.twitter.txt"), "rb", encoding = "UTF-8")
-    
-    # Create a write connection to sample files
-    blogs.sample <- file(paste0(path.to.data, "blogs_sample.txt"), "w")
-    news.sample <- file(paste0(path.to.data, "news_sample.txt"), "w")
-    twitter.sample <- file(paste0(path.to.data, "twitter_sample.txt"), "w")
-    
-    # Get the number of lines in each file
-    blogs.n <- countLines(paste0(path.to.data, "en_US.blogs.txt"))
-    news.n <- countLines(paste0(path.to.data, "en_US.news.txt"))
-    twitter.n <- countLines(paste0(path.to.data, "en_US.twitter.txt"))
-    
-    p <- 0.25    # Proportion of text to sample
-    lines.n <- 1    #Number of lines to sample at a time
-    
-    # Set seed
-    set.seed(110317)
-    
-    # Sample texts
+# Unzip the dataset
+#unzip("Coursera-Swiftkey.zip", exdir = "data", overwrite = FALSE, setTimes = FALSE)
+path.to.data <- paste0(getwd(), "/data/final/en_US/")
 
-    sample.line <- rbinom(blogs.n, 1, p)
-    for (i in 1:blogs.n) {
-        line <- readLines(blogs.con, n = lines.n, skipNul = TRUE, encoding = "UTF-8")
-        if (sample.line[i]) writeLines(line, con = blogs.sample)
+# Create a read connection to each English dataset (Twitter, news, and blogs)
+blogs.con <- file(paste0(path.to.data, "en_US.blogs.txt"), "rb", encoding = "UTF-8")
+news.con <- file(paste0(path.to.data, "en_US.news.txt"), "rb", encoding = "UTF-8")
+twitter.con <- file(paste0(path.to.data, "en_US.twitter.txt"), "rb", encoding = "UTF-8")
+
+# Create a write connection to testing data
+train <- lapply(paste0(getwd(), "/data/final/en_US/train_", 1:10, ".txt"), file, "w")
+test <- file(paste0(path.to.data, "test.txt"), "w")
+
+# Get the number of lines in each file
+blogs.n <- countLines(paste0(path.to.data, "en_US.blogs.txt"))
+news.n <- countLines(paste0(path.to.data, "en_US.news.txt"))
+twitter.n <- countLines(paste0(path.to.data, "en_US.twitter.txt"))
+
+p <- 0.8    # Proportion of text to sample
+
+# Set seed
+set.seed(110317)
+
+# Split texts into training and testing sets. Write the training data to multiple files so that tokenization can be done in chunks
+splitTexts <- function(read.con, lines.n) {
+    is.training <- rbinom(lines.n, 1, p)
+    file.number <- rep_along(is.training, 1:10)
+    for (i in 1:lines.n) {
+        line <- readLines(read.con, n = 1, skipNul = TRUE, encoding = "UTF-8")
+        if (is.training[i]) {
+            writeLines(line, con = train[[file.number[i]]])
+        } else {
+            writeLines(line, con = test)
+        }
     }
-    
-    sample.line <- rbinom(news.n, 1, p)
-    for (i in 1:news.n) {
-        line <- readLines(news.con, n = lines.n, skipNul = TRUE, encoding = "UTF-8")
-        if (sample.line[i]) writeLines(line, con = news.sample)
-    }
-    
-    sample.line <- rbinom(twitter.n, 1, p)
-    for (i in 1:twitter.n) {
-        line <- readLines(twitter.con, n = lines.n, skipNul = TRUE, encoding = "UTF-8")
-        if (sample.line[i]) writeLines(line, con = twitter.sample)
-    }
-    
-    
-    # Close file connections
-    close(blogs.con)
-    close(news.con)
-    close(twitter.con)
-    
-    close(blogs.sample)
-    close(news.sample)
-    close(twitter.sample)
-    
-    n.lines <- data.frame(dataset = c("blogs", "news", "twitter"), n.lines = c(blogs.n, news.n, twitter.n), proportion.sampled = c(p, p, p))
-    return(n.lines)
+    close(read.con)
 }
+
+splitTexts(blogs.con, blogs.n)
+splitTexts(news.con, news.n)
+splitTexts(twitter.con, twitter.n)
+
+# Close file connections
+invisible(lapply(train, close))
+close(test)
